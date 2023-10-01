37e6b64c99339815befe20209b7497cff821b708
SofiaGL082 <sogiraldo@unal.edu.co>
2023-10-01 18:00:24 -0500

    :100:

--- src/test/java/vehiculos/Automovil.java
+++ src/test/java/vehiculos/Automovil.java
@@ -7,9 +7,9 @@ public class Automovil extends Vehiculo{
 	private int puestos;
 	private static List<Automovil> listado = new ArrayList<Automovil>();
 	
-	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
+	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puesto) {
 		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
-		this.puestos = puestos;
+		this.puestos = puesto;
 		Automovil.listado.add(this);
 	}
 	
